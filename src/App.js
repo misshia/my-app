0d9b04fb0fbb29cc1b06e47ced5e7dc3e4fbb9a9
Misia <kozielska.mari@gmail.com>
2021-02-19 11:11:39 +0100

    math.round

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Weather city = "Paris"/>
+        <Weather />
         <a
           className="App-link"
           href="https://reactjs.org"
