8bb40440c146fa40af029fb3fdf0e41e88f78637
Misia <kozielska.mari@gmail.com>
2021-02-19 13:51:08 +0100

    mods terminal

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function Weather () {
 
     function handleResponse(response) {
-    alert (`The weather in New York is Math.round(${response.data.main.temp}) C`); 
+    alert (`The weather in New York is ${response.data.main.temp} C`); 
     }
     let apiKey = "88e0dcff954aec9e80363d859d6d1b98";
     let apiUrl  = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
